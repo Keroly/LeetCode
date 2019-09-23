0b507e2aae6171ff8d24924b825dead3a4317f77
Keroly <15122112779@163.com>
2019-09-23 19:49:40 +0800

    MaxGap

--- algorithm/MaxGap.java
+++ algorithm/MaxGap.java
@@ -2,24 +2,9 @@
 
 public class MaxGap {
 
-    // only for 0~200 value
     public static void MaxGap(int[] arr){
         if (arr.length < 2 || arr == null){
             return;
-        }
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++){
-            max = Math.max(max,arr[i]);
-        }
-        int[] bucket = new int[max + 1];
-        for (int i = 0; i < arr.length; i++){
-            bucket[arr[i]]++;
-        }
-        int i = 0;
-        for (int j = 0;j < bucket.length; j++){
-            while (bucket[j]-- > 0){
-                arr[i++] = j;
-            }
         }
     }
 
