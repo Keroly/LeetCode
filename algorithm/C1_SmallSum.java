902a4263a0c7ce1e2a130aae571c9fcf1d36c92f
Keroly <15122112779@163.com>
2019-10-22 20:02:31 +0800

    SmallSort

---
+++ algorithm/C1_SmallSum.java
@@ -0,0 +1,55 @@
+// 给定一个数组,对于数组中的每个元素,找出位于他的左边&&比它小的元素并求和,累加
+public class C1_SmallSum {
+
+    public static int smallSum(int[] arr){
+        if (arr.length < 2 || arr == null){
+            return 0;
+        }
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static int mergeSort(int[] arr, int L, int R){
+        if (L == R){
+            return 0;
+        }
+        int mid = L + ((R - L) >> 1);
+        return mergeSort(arr, L, mid)
+                + mergeSort(arr, mid + 1, R)
+                + merge(arr,L,mid,R);
+    }
+
+    private static int merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        int res = 0;
+        while (p1 <= m && p2 <= r){
+            res += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m){
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r){
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++){
+            arr[l + i] = help[i];
+        }
+        return res;
+    }
+
+    public static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,5,4,8,10};
+        int res = smallSum(arr);
+        System.out.println(res);
+    }
+}
+
