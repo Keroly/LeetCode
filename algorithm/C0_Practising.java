f84afae3ce63e58e56af7c4f49f5c5d4db247d9c
Keroly <15122112779@163.com>
2019-10-23 09:17:00 +0800

    Practising

--- algorithm/C0_Practising.java
+++ algorithm/C0_Practising.java
@@ -1,38 +1,30 @@
 public class C0_Practising {
 
-    public static void sort(int[] arr,int l, int r){
+    public static int[] partition(int[] arr,int l, int r){
 
-        if(arr.length < 2 || arr == null){
-            return;
+        int less = l - 1;
+        int more = r;
+
+        while(l < more){
+            if(arr[l] < arr[r]){
+                swap(arr, ++less, l++); // 这里的 ++  很有必要
+            }else if (arr[l] > arr[r]){
+                swap(arr,--more,l);
+            }else{
+                l++;
+            }
         }
-
-        mergesort(arr,l,r);
+        swap(arr,more,r);
+        return new int[] {less + 1, more};
     }
 
-    public static void  mergesort(int[] arr,int l,int r){
-        if(l == r) return;
-        int mid = l + ((r - l) >> 1);
-        mergesort(arr,l,mid);
-        mergesort(arr,mid + 1,r);
-        outSort(arr,l,mid,r);
-    }
-
-    private static void outSort(int[] arr, int l, int mid, int r) {
-        int[] help = new int[r - l + 1];
-        int i = 0;
-        int p1 = l;
-        int p2 = mid + 1;
-        while (p1 <= mid && p2 <=r){
-            help[i++] = arr[p1] > arr[p2] ? arr[p2++] : arr[p1++];
-        }
-        while (p1 <= mid){
-            help[i++] = arr [p1++];
-        }
-        while (p2 <= r){
-            help[i++] = arr [p2++];
-        }
-        for(i = 0; i < help.length; i++){
-            arr[l + i] = help[i];
+    public static void sort(int[] arr, int l, int r){
+        if(l < r){
+            int standard_index = l + (int)(Math.random() * (r - l + 1));
+            swap(arr, standard_index, r);
+            int result[] = partition(arr,l,r);
+            sort(arr, l, result[0] - 1);
+            sort(arr, result[0] + 1,r);
         }
     }
 
@@ -44,7 +36,7 @@ public class C0_Practising {
 
     public static void main(String[] args) {
         int[] arr = new int[]{21,37,6,25,19,18,20,30};
-        sort(arr,0,arr.length-1);
+        sort(arr, 0, arr.length - 1);
         for (int x:arr) {
             System.out.print(x + " ");
         }
