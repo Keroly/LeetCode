e4515dcaf9d6b23f4b0cf34a27252b4dba77b820
Keroly <15122112779@163.com>
2019-11-13 16:48:01 +0800

    最长无重复字符子串

---
+++ leetcode/T0003.java
@@ -0,0 +1,54 @@
+/*
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+
+import java.util.HashMap;
+import java.util.LinkedList;
+public class T0003 {
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == "") return 0;
+        LinkedList<Character> list = new LinkedList<Character>();
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        char[] arr = s.toCharArray();
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i])){
+                list.addLast(arr[i]);
+                map.put(arr[i],i);
+            }else {
+                while (list.getFirst() != arr[i]){
+                    res = res > list.size() ? res : list.size();
+                    map.remove(list.peekFirst());
+                    list.pollFirst();
+                }
+                map.remove(list.peekFirst());
+                list.pollFirst();
+                list.addLast(arr[i]);
+                map.put(arr[i],i);
+            }
+        }
+        return res > list.size() ? res : list.size();
+    }
+
+    public static void main(String[] args) {
+        String str = "aac";
+        System.out.println(lengthOfLongestSubstring(str));
+    }
+}
