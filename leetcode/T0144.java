c72a8b755b6a56acbce7b2629b35341573effd94
Keroly <15122112779@163.com>
2019-11-26 11:15:55 +0800

    非递归先序遍历

--- leetcode/T0144.java
+++ leetcode/T0144.java
@@ -30,17 +30,16 @@ public class T0144 {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<Integer>();
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        if(root != null){
-            stack.push(root);
-            while (!stack.isEmpty()){
-                TreeNode node = stack.pop();
-                list.add(node.val);
-                if(node.right != null){
-                    stack.push(node.right);
-                }
-                if(node.left != null){
-                    stack.push(node.left);
-                }
+        if (root == null) return list;
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode top = stack.pop();
+            list.add(top.val);
+            if (top.right != null){
+                stack.push(top.right);
+            }
+            if (top.left != null){
+                stack.push(top.left);
             }
         }
         return list;
