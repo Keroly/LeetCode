96527d2e9680f07374d8b1b8426dbdca143f19c8
Keroly <15122112779@163.com>
2019-11-29 08:34:53 +0800

    替换空格

--- offer/B02.java
+++ offer/B02.java
@@ -1,19 +1,28 @@
 /*
     替换空格
-题目描述
+题目描述:(从后往前）
 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy，则经过替换之后的字符串为We%20Are%20Happy。
  */
 public class B02 {
     public static String replaceSpace(StringBuffer str) {
-        StringBuffer string = new StringBuffer();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != ' '){
-                string.append(str.charAt(i));
-            }else {
-                string.append("%20");
+        char[] array = str.toString().toCharArray();
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == ' '){
+                count++;
             }
         }
-        return string.toString();
+        char[] result = new char[array.length + 2 * count];
+        for (int i = array.length - 1, j = array.length + 2 * count - 1; i >= 0 && j >= 0; i--,j--) {
+            if (array[i] != ' '){
+                result[j] = array[i];
+            }else {
+                result[j--] = '0';
+                result[j--] = '2';
+                result[j] = '%';
+            }
+        }
+        return String.valueOf(result);
     }
 
     public static void main(String[] args) {
