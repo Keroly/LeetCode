57ca314dbc31f4c8e696caf8147b20c6e9c7599b
Keroly <15122112779@163.com>
2019-11-30 19:12:44 +0800

    旋转数组的最小数字

--- offer/B06.java
+++ offer/B06.java
@@ -10,19 +10,27 @@ NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
 public class B06 {
     public static int minNumberInRotateArray(int [] array) {
         if (array.length == 0) return 0;
-        int i = 0, j = array.length - 1;
-        while (i != j - 1){
-            int mid = (j + i) / 2;
-            if (array[mid] >= array[i]){
-                i = mid;
-            }else if (array[mid] < array[j]){
-                j = mid;
-            }
+        int low = 0;
+        int high = array.length - 1;
+        int mid = 0;
+        while(low < high){
+            if (array[low] < array[high]) // 如果array本身就是排好序的
+                return array[low];
+            mid = low + (high - low) / 2;
+            if(array[mid] > array[low]) // low 向右移动
+                low = mid + 1; // 如果mid属于左侧，那么mid可以直接排除，因为答案一定在右侧
+            else if(array[mid] < array[high]) // high 向左移动
+                high = mid; // 如果mid属于右侧，那么mid不可以排除，因为答案一定在右侧
+            else low++; // 如果array[mid]等于array[low / high]中的任何一个都把low右移重新计算mid，因为如果low位置是唯一答案应该早已被判断出  1234 / 1111
         }
-        return array[j];
+        return array[low];
     }
     public static void main(String[] args) {
-        int[] arr = {3,4,5,1,1,3};
+        int[] arr = {1,0,1,1,1};
+        int[] arr1 = {0,1,1,1,1};
+        int[] arr2 = {1,1,1,1,0};
         System.out.println(minNumberInRotateArray(arr));
+        System.out.println(minNumberInRotateArray(arr1));
+        System.out.println(minNumberInRotateArray(arr2));
     }
 }
