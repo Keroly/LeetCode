e19052cf18fdc03a11f902da17f0af26b09f6565
Keroly <15122112779@163.com>
2020-02-28 14:23:52 +0800

    斐波那契数列

--- offer/B07.java
+++ offer/B07.java
@@ -6,14 +6,14 @@
 
 public class B07 {
     public static int Fibonacci(int n) { // 递归的时间复杂度：O(2^n) 空间：O(1)     此方法时间:O(n) 空间:O(1)
-        if (n < 2) return n;
-        int a = 0;
-        int b = 1;
-        for (int i = 2; i <= n; i++){
-            b = a + b;
-            a = b - a;
-        }
-        return b;
+       if (n < 2) return n;
+       int a = 0;
+       int b = 1;
+       for (int i = 1; i < n; i++){
+           b = a + b;
+           a = b - a;
+       }
+       return b;
     }
 
     public static void main(String[] args) {
