1e9159268409c1232d628526648c2ced0d48e5cd
Keroly <15122112779@163.com>
2020-02-28 14:42:06 +0800

    矩形覆盖

--- offer/B10.java
+++ offer/B10.java
@@ -4,28 +4,17 @@
 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
  */
 public class B10 {
-    public static int sum = 0;
-    public static int process(int curr, int target){
-
-        if (curr == target){
-            sum += 1;
-            return sum;
-        }
-        if (curr == target - 1){
-            sum += 2;
-            return sum;
-        }
-        process(curr + 1, target);
-        process(curr + 2, target);
-        return sum;
-    }
-
     public static int RectCover(int target) {
         if (target <= 0) return 0;
-        return process(1, target);
+
+        if (target == 1) return 1;
+
+        if (target == 2) return 2;
+
+        return RectCover(target - 1) + RectCover(target - 2);
     }
 
     public static void main(String[] args) {
-        System.out.println(RectCover(2));
+        System.out.println(RectCover(3));
     }
 }
