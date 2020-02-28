cd97fa6161274a78dd5d95188bd1698205db27d7
Keroly <15122112779@163.com>
2020-02-28 16:04:21 +0800

    二进制中1的个数

--- offer/B20.java
+++ offer/B20.java
@@ -4,14 +4,21 @@
 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
  */
 public class B20 {
-    public static int NumberOf1(int n) {
+//    public static int NumberOf1(int n) {  // 挪动标准
+//        int x = 1;
+//        int count = 0;
+//        while (x != 0){
+//            if ((n & x) != 0) count++;
+//            x = x << 1;
+//        }
+//        return count;
+//    }
+
+    public static int NumberOf1(int n) {    // 消灭原数
         int count = 0;
-        int flag = 1;
-        while (flag != 0){
-            if ((n & flag ) != 0){
-                count++;
-            }
-            flag = flag << 1;
+        while (n != 0){
+            count++;
+            n = (n - 1) & n;
         }
         return count;
     }
