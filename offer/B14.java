db861fe95c8c82a65e422d04b87fdf926f6a9c49
Keroly <15122112779@163.com>
2020-02-29 17:29:56 +0800

    合并两个有序链表

--- offer/B14.java
+++ offer/B14.java
@@ -15,29 +15,19 @@ public class B14 {
     }
 
     public static ListNode Merge(ListNode list1,ListNode list2) {
-        ListNode node = new ListNode(-1);
-        ListNode cur = node;
-
-        while (list1 != null && list2 != null){
-            if (list1.val >= list2.val){
-                cur.next = list2;
-                list2 = list2.next;
-            }else {
-                cur.next = list1;
-                list1 = list1.next;
-            }
-            cur = cur.next;
+        if(list1 == null){
+            return list2;
         }
-
-        if (list1 != null){
-            cur.next = list1;
+        if(list2 == null){
+            return list1;
         }
-
-        if (list2 != null){
-            cur.next = list2;
+        if(list1.val <= list2.val){
+            list1.next = Merge(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = Merge(list1, list2.next);
+            return list2;
         }
-
-        return node.next;
     }
 
     public static void main(String[] args) {
