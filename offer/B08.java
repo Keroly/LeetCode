e5a5b295ab965276e4cf58aeb6f2999ace64b784
Keroly <15122112779@163.com>
2020-03-26 10:15:37 +0800

    跳台阶

--- offer/B08.java
+++ offer/B08.java
@@ -1,20 +1,23 @@
 /*
     跳台阶
-题目描述
+
+题目描述：
 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
  */
-public class B08 {
-    public static int JumpFloor(int target) {
-        if (target == 1){
-            return 1;
-        }
-        if (target == 2){
-            return 2;
-        }
-        return JumpFloor(target - 1) + JumpFloor(target - 2);
-    }
 
-    public static void main(String[] args) {
-        System.out.println(JumpFloor(4));
+public class B08 {
+    public int JumpFloor(int target) {
+        if (target <= 0) return 0;
+        if (target == 1) return 1;
+        int a = 1;
+        int b = 2;
+        while (target-- != 2){
+            b = a + b;
+            a = b - a;
+        }
+        return b;
     }
 }
+
+// 时间：O(n)  空间：O(1)
+
