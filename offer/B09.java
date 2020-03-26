93220d5697b30797a779c934e298ff67f1216fbe
Keroly <15122112779@163.com>
2020-03-26 10:53:19 +0800

    变态跳台阶

--- offer/B09.java
+++ offer/B09.java
@@ -1,6 +1,7 @@
 /*
     变态跳台阶
-题目描述
+
+题目描述：
 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
  */
 
@@ -8,11 +9,9 @@
 // f(n - 1) = f(n - 2) + f(n - 3) + ... + f(1)
 // f(n) - f(n - 1) = f(n - 1)
 // f(n) = 2 f(n - 1) = 4 f(n - 2) = 8 f(n - 3) = 2 ^ (n - 1) f(1) = 2 ^ (n - 1)
+
 public class B09 {
-    public  static int JumpFloorII(int target) {
+    public  int JumpFloorII(int target) {
         return (int)Math.pow(2, target -1);
-    }
-    public static void main(String[] args) {
-        System.out.println(JumpFloorII(2));
     }
 }
