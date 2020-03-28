121168e476359dcb365200d71943ba1c399cb055
Keroly <15122112779@163.com>
2020-03-28 11:40:10 +0800

    链表倒数第k个结点

--- offer/B12.java
+++ offer/B12.java
@@ -1,10 +1,12 @@
 /*
     链表中倒数第k个节点
-题目描述
+
+题目描述：
 输入一个链表，输出该链表中倒数第k个结点。
  */
+
 public class B12 {
-    public static class ListNode {
+    public class ListNode {
         int val;
         ListNode next = null;
 
@@ -13,33 +15,26 @@ public class B12 {
         }
     }
 
-    public static ListNode FindKthToTail(ListNode head,int k) {
-         if (head == null || k <= 0) return null;
-         ListNode start = head;
-         ListNode end = head;
+    public ListNode FindKthToTail(ListNode head,int k) {
+        if (head == null) return null;
+        if (k <= 0) return null;
+        ListNode slow = head;
+        ListNode fast = head;
 
-         for (int i = 1; i < k; i++){
-             end = end.next;
-         }
-
-         while (end != null && end.next != null){
-             end = end.next;
-             start = start.next;
-         }
-
-         if (end == null) return end;
-
-         return start;
-    }
-
-    public static void main(String[] args) {
-        ListNode node = new ListNode( 1);
-        node.next = new ListNode(2);
-        node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-        node = FindKthToTail(node,1);
-        if (node != null){
-            System.out.println(node.val);
+        while (fast != null && --k > 0){
+            fast = fast.next;
         }
+
+        if (fast == null){
+            return null;
+        }
+
+        while (fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
     }
+
 }
