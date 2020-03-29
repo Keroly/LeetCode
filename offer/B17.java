55754935258a3918e8834dc625526f113511a34e
Keroly <15122112779@163.com>
2020-03-29 21:53:18 +0800

    二叉树的镜像

--- offer/B17.java
+++ offer/B17.java
@@ -1,8 +1,10 @@
 /*
     二叉树的镜像
-题目描述
+
+题目描述：
 操作给定的二叉树，将其变换为源二叉树的镜像。
  */
+
 public class B17 {
     public class TreeNode {
         int val = 0;
@@ -12,13 +14,13 @@ public class B17 {
             this.val = val;
         }
     }
+
     public void Mirror(TreeNode root) {
         if (root == null) return;
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        root.left = right;
-        root.right = left;
-        Mirror(left);
-        Mirror(right);
+        TreeNode node = root.left;
+        root.left = root.right;
+        root.right = node;
+        Mirror(root.left);
+        Mirror(root.right);
     }
 }
