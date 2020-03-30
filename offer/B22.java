91a12d3e1eac00e87699638b4b8fc8d4f17fea07
Keroly <15122112779@163.com>
2020-03-30 11:21:57 +0800

    包含min函数的栈

--- offer/B22.java
+++ offer/B22.java
@@ -1,14 +1,16 @@
 /*
     包含min函数的栈
-题目描述
+
+题目描述：
 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
  */
 
 import java.util.Stack;
+
 public class B22 {
-    static Stack<Integer> data = new Stack();
-    static Stack<Integer> min = new Stack();
-    public static void push(int node) {
+    Stack<Integer> data = new Stack();
+    Stack<Integer> min = new Stack();
+    public void push(int node) {
         data.push(node);
         if (min.isEmpty()) min.push(node);
         if (node < min.peek()){
@@ -18,36 +20,25 @@ public class B22 {
         }
     }
 
-    public static void pop() {
+    public void pop() {
         if (!data.isEmpty() && !min.isEmpty()){
             data.pop();
             min.pop();
         }
     }
 
-    public static int top() {
+    public int top() {
         if (!data.isEmpty()){
             return data.peek();
         }
         return -1;
     }
 
-    public static int min() {
+    public int min() {
         if (!min.isEmpty()){
             return min.peek();
         }
         return -1;
     }
 
-    public static void main(String[] args) {
-        B22 stack = new B22();
-        stack.push(3);
-        stack.push(2);
-        stack.push(1);
-        stack.push(2);
-        stack.push(2);
-        stack.pop();
-        stack.pop();
-        System.out.println(B22.min());
-    }
 }
