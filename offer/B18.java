aecaf7070f6358d66ff894b9875b5237ee41dfde
Keroly <15122112779@163.com>
2020-03-30 11:19:29 +0800

    顺时针打印矩阵

--- offer/B18.java
+++ offer/B18.java
@@ -1,30 +1,31 @@
 /*
     顺时针打印矩阵
-题目描述
+
+题目描述：
 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
 例如，如果输入如下4 X 4矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
  */
 
 import java.util.ArrayList;
+
 public class B18 {
-    public static ArrayList<Integer> printMatrix(int [][] matrix) { // 矩阵通用解法
+    public ArrayList<Integer> printMatrix(int [][] matrix) { // 矩阵通用解法
         ArrayList<Integer> list = new ArrayList();
+        if (matrix == null) return list;
         int calm_size = matrix.length;
         int vertical_size = matrix[0].length;
         int i = -1;
         int j = -1;
-        while (calm_size > 0 && vertical_size >0){
+        while (calm_size > 0 && vertical_size > 0){
             i++;
             j++;
-            while (calm_size == 1 && vertical_size > 0 ){
+            while (calm_size == 1 && vertical_size-- > 0){
                 list.add(matrix[i][j++]);
-                vertical_size--;
             }
-            while (calm_size > 0 && vertical_size == 1){
+            while (vertical_size == 1 && calm_size-- > 0){
                 list.add(matrix[i++][j]);
-                calm_size--;
             }
-            if (calm_size > 0 && vertical_size >0){
+            if (calm_size > 0 && vertical_size > 0){
                 for (int k = 0; k < vertical_size - 1; k++){
                     list.add(matrix[i][j + k]);
                 }
@@ -44,12 +45,4 @@ public class B18 {
         return list;
     }
 
-
-    public static void main(String[] args) {
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-        ArrayList list = printMatrix(matrix);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
-    }
 }
