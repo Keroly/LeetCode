492d2761ecd385d2d3e4754e55d836e492f81202
Keroly <15122112779@163.com>
2020-04-01 13:08:07 +0800

    二叉搜索树的后序遍历序列

--- offer/B24.java
+++ offer/B24.java
@@ -1,37 +1,39 @@
 /*
     二叉搜索树的后序遍历序列
-题目描述
+
+题目描述：
 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
  */
+
 public class B24 {
-    public static boolean process(int [] sequence, int i, int j){
+    public boolean process(int [] sequence, int i, int j){
         if (i >= j) return true;
 
-        int x = i;
+        int flag = i;
 
-        for (;x < j; x++){
-            if (sequence[x] > sequence[j]) break;
+        while (flag < j){
+            if (sequence[flag] > sequence[j]) {
+                break;
+            }
+            flag += 1;
         }
 
-        for (int y = x; y < j; y++){
-            if (sequence[y] < sequence[j]) return false;
+        int tag = flag;
+
+        while (flag < j){
+            if (sequence[flag] < sequence[j]){
+                return false;
+            }
+            flag += 1;
         }
 
-        return process(sequence, i, x - 1) && process(sequence, x, j - 1);
+        return process(sequence, i, flag - 1) && process(sequence, flag, j - 1);
 
     }
 
-    public static boolean VerifySquenceOfBST(int [] sequence) {
+    public boolean VerifySquenceOfBST(int [] sequence) {
         if (sequence == null || sequence.length == 0) return false;
         return process(sequence, 0, sequence.length - 1);
     }
 
-    public static void main(String[] args) {
-        int[] sequence = {5,7,6,9,11,10,8};
-        int[] sequence_1 = {5};
-        int[] sequence_2 = {7,4,6,5};
-        System.out.println(VerifySquenceOfBST(sequence));
-        System.out.println(VerifySquenceOfBST(sequence_1));
-        System.out.println(VerifySquenceOfBST(sequence_2));
-    }
 }
