eb553eb340777c4010811e81558b374653eaf7d2
Keroly <15122112779@163.com>
2020-04-02 21:28:26 +0800

    二叉搜索树和双向链表

--- offer/B35.java
+++ offer/B35.java
@@ -1,10 +1,14 @@
 /*
     二叉搜索树和双向链表
-题目描述
+
+题目描述：
 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
  */
+
+import java.util.Stack;
+
 public class B35 {
-    public static class TreeNode {
+    public class TreeNode {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
@@ -15,44 +19,29 @@ public class B35 {
         }
     }
 
-    public static TreeNode node;
-
-    public static void process(TreeNode root){
-        if (root == null) return;
-        if (root.left != null){
-            process(root.left);
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        if (pRootOfTree == null) return null;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = pRootOfTree;
+        TreeNode result = null;
+        TreeNode pre = null;
+        while (!stack.isEmpty() || cur != null){
+            if (cur != null){
+                stack.add(cur);
+                cur = cur.left;
+            }else {
+                TreeNode node = stack.pop();
+                if (result == null){
+                    result = node;
+                }
+                if (pre != null){
+                    pre.right = node;
+                }
+                node.left = pre;
+                pre = node;
+                cur = node.right;
+            }
         }
-        root.left = node;
-        if (node != null){
-            node.right = root;
-        }
-        node = root;
-        if (root.right != null){
-            process(root.right);
-        }
-    }
-
-    public static TreeNode Convert(TreeNode pRootOfTree) {
-        process(pRootOfTree);
-        while (node != null && node.left != null){
-            node = node.left;
-        }
-        return node;
-    }
-
-    public static void main(String[] args) {
-        TreeNode root1 = new TreeNode(8);
-        root1.left = new TreeNode(5);
-        root1.right = new TreeNode(10);
-        root1.left.left = new TreeNode(3);
-        root1.left.right = new TreeNode(6);
-        root1.right.left = new TreeNode(9);
-        root1.right.right = new TreeNode(12);
-
-        Convert(root1);
-        while (node != null){
-            System.out.print(node.val + " ");
-            node = node.right;
-        }
+        return result;
     }
 }
