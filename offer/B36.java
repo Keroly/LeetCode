b79eb602fc1b2f44cbd6591a16f524c2d9b1c130
Keroly <15122112779@163.com>
2020-04-03 21:08:49 +0800

    字符串的排列

--- offer/B36.java
+++ offer/B36.java
@@ -1,11 +1,11 @@
 /*
     字符串的排列
+
 题目描述:
 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
 输入描述:
 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
  */
-
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,16 +18,19 @@ public class B36 {
     }
 
     public void process(ArrayList list, int index, char[] arr){
-        if (index == arr.length - 1) list.add(new String(arr));
+        if (index == arr.length - 1) {
+            list.add(new String(arr));
+            return;
+        }
+
         for (int i = index; i < arr.length; i++){
-            if (i == index || arr[i] != arr[index]){
-                swap(arr, i, index);
-                process(list, index + 1, arr);
-                swap(arr, i, index);
-            }
+           if (i == index || arr[i] != arr[index]){
+               swap(arr, i, index);
+               process(list, index + 1, arr);
+               swap(arr, i, index);
+           }
         }
     }
-
 
     public ArrayList<String> Permutation(String str) {
         ArrayList<String> list = new ArrayList<>();
