b2a3c578503a2181a3d7116ddd409154023a3b4e
Keroly <15122112779@163.com>
2020-04-04 19:23:04 +0800

    最小的k个数

--- offer/B37.java
+++ offer/B37.java
@@ -1,46 +1,35 @@
 /*
     最小的K个数
-题目描述
+
+题目描述：
 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
  */
 
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Comparator;
-public class B37 {
 
+public class B37 {
     public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
         ArrayList<Integer> result = new ArrayList<Integer>();
-        if(k > input.length || k == 0){
-            return result;
-        }
+        if (input == null || k <= 0 || k > input.length) return result;
         PriorityQueue<Integer> queue = new PriorityQueue<>(k, new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                return o2.compareTo(o1);
+                return o2 - o1;
             }
         });
         for (int i = 0; i < input.length; i++){
-            if (queue.size() < k){
-                queue.offer(input[i]);
-            }else {
-                if (queue.peek() > input[i]){
-                    queue.poll();
-                    queue.offer(input[i]);
-                }
+            if (queue.size() < k) {
+                queue.add(input[i]);
+                continue;
+            }
+            if (input[i] < queue.peek()){
+                queue.poll();
+                queue.add(input[i]);
             }
         }
-        for (Integer integer: queue) {
-            result.add(integer);
-        }
+        result = new ArrayList<>(queue);
         return result;
-    }
-
-    public static void main(String[] args) {
-        int[] array = {8,3,5,11,7};
-        ArrayList list = new B37().GetLeastNumbers_Solution(array,2);
-        for (int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
-        }
     }
 }
