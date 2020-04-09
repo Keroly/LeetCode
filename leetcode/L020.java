d7b40f81ae1158e8b4da58068965226ab419d664
Keroly <15122112779@163.com>
2020-04-09 16:53:45 +0800

    有效的括号

--- leetcode/L020.java
+++ leetcode/L020.java
@@ -9,9 +9,31 @@
 左括号必须以正确的顺序闭合。
  */
 
-import java.util.HashMap;
 import java.util.Stack;
 
 public class L020 {
+    public boolean isValid(String s) {
+        if (s == null) return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '('){
+                stack.add(')');
+            }else if (s.charAt(i) == '['){
+                stack.add(']');
+            }else if (s.charAt(i) == '{'){
+                stack.add('}');
+            }else {
+                if (!stack.isEmpty() && stack.pop() == s.charAt(i)){
+                    continue;
+                }
+                return false;
+            }
+        }
 
+        if (stack.isEmpty()){
+            return true;
+        }
+
+        return false;
+    }
 }
