27dcc50940ab3ac38815153b8fc264359daa0863
Keroly <15122112779@163.com>
2020-04-10 15:20:53 +0800

    爬楼梯

---
+++ leetcode/L070.java
@@ -0,0 +1,22 @@
+/*
+    爬楼梯
+
+题目描述：
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ */
+
+public class L070 {
+    public int climbStairs(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int a = 1;
+        int b = 2;
+        for (int i = 3; i <= n; i++){
+            b = a + b;
+            a = b - a;
+        }
+        return b;
+    }
+}
