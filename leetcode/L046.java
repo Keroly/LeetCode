129f40a0aa9b9b86c37f13c0fe00d6d4bfa80cf4
Keroly <15122112779@163.com>
2020-04-12 11:52:51 +0800

    全排列

---
+++ leetcode/L046.java
@@ -0,0 +1,36 @@
+/*
+    全排列
+
+题目描述：
+给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class L046 {
+    public void process(ArrayList<Integer> nums, int length, int index, List<List<Integer>> result){
+        if (index == length){
+            result.add(new ArrayList<>(nums));
+        }
+        for (int i = index; i < length; i++){
+            if (i == index || nums.get(i) != nums.get(index)){
+                Collections.swap(nums, index, i);
+                process(nums, length, index + 1, result);
+                Collections.swap(nums, index, i);
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) return result;
+        ArrayList<Integer> elements = new ArrayList<Integer>();
+        for (Integer element: nums) {
+            elements.add(element);
+        }
+        process(elements, nums.length, 0, result);
+        return result;
+    }
+}
