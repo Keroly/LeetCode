10fd9a20c26146ac648eeb220b81af2a31347063
Keroly <15122112779@163.com>
2020-04-16 22:51:27 +0800

    数组的逆序对

--- offer/B43.java
+++ offer/B43.java
@@ -10,46 +10,5 @@
  */
 
 public class B43 {
-    int count = 0;
-    public void process(int[] array, int[] help, int left, int right){
-        if (left == right) return;
-        int mid = (left + right) >> 1;
-        process(array, help, left, mid);
-        process(array, help, mid + 1, right);
-        int p_left = mid;
-        int p_right = right;
-        int p_help = right;
 
-        while (p_left >= left && p_right > mid){
-            if (array[p_left] > array[p_right]){
-                count += (p_right - mid);
-                if(count >= 1000000007)     // 数值过大求余
-                {
-                    count %= 1000000007;
-                }
-                help[p_help--] = array[p_left--];
-            }else {
-                help[p_help--] = array[p_right--];
-            }
-        }
-
-        while (p_left >= left) {
-            help[p_help--] = array[p_left--];
-        }
-
-        while (p_right > mid){
-            help[p_help--] = array[p_right--];
-        }
-
-        for (int i = left; i <= right; i++){
-            array[i] = help[i];
-        }
-    }
-
-    public int InversePairs(int[] array) {
-        if (array == null || array.length <= 1) return 0;
-        int[] help = new int[array.length];
-        process(array, help, 0, array.length - 1);
-        return count % 1000000007;
-    }
 }
