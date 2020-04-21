c66ab3b832d9145b5a55c5782b9d802759f8e993
Keroly <15122112779@163.com>
2020-04-21 17:00:36 +0800

    二叉树的深度

---
+++ offer/J55_1.java
@@ -0,0 +1,28 @@
+/*
+    二叉树的深度
+
+题目描述：
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+ */
+
+public class J55_1 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
