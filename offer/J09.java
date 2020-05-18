e190bfa1faf0df4379f99b5e59a1c4618b8c6c28
Keroly <15122112779@163.com>
2020-05-18 08:15:53 +0800

    两个栈实现队列

--- offer/J09.java
+++ offer/J09.java
@@ -1,5 +1,5 @@
 /*
-    两个栈实现队列
+    两个栈实现队列(n)
 
 题目描述：
 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。
@@ -7,6 +7,31 @@
 
  */
 
-public class J09 {
+import java.util.Stack;
 
+public class J09 {
+    Stack<Integer> st_1;
+    Stack<Integer> st_2;
+
+    public J09() {
+        st_1 = new Stack();
+        st_2 = new Stack();
+    }
+
+    public void appendTail(int value) {
+        st_1.push(value);
+    }
+
+    public int deleteHead() {
+        if (st_2.isEmpty()){
+            while (!st_1.isEmpty()){
+                st_2.push(st_1.pop());
+            }
+        }
+        if (!st_2.isEmpty()) {
+            return st_2.pop();
+        }else {
+            return -1;
+        }
+    }
 }
