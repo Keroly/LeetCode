36ae1ca0c481f28f9feb9d113c99a365387c5327
Keroly <15122112779@163.com>
2020-05-19 09:02:30 +0800

    合并两个排序的链表

---
+++ offer/J25.java
@@ -0,0 +1,48 @@
+/*
+    合并两个排序的链表(n)
+
+题目描述：
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+ */
+
+public class J25 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null){
+            return l2;
+        }else if (l2 == null) {
+            return l1;
+        }
+        ListNode head = l1;
+        if (l1.val > l2.val){
+            head = l2;
+            l2 = l2.next;
+        }else {
+            l1 = l1.next;
+        }
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return head;
+    }
+}
