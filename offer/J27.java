7d723fe06d6b3b17f6231ff91cfee0bcdb560982
Keroly <15122112779@163.com>
2020-05-19 10:04:21 +0800

    二叉树的镜像

---
+++ offer/J27.java
@@ -0,0 +1,27 @@
+/*
+    二叉树的镜像
+
+题目描述：
+操作给定的二叉树，将其变换为源二叉树的镜像。
+
+ */
+
+public class J27 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = mirrorTree(right);
+        root.right = mirrorTree(left);
+        return root;
+    }
+}
