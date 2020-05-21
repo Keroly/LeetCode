0ec017399ca874eacac90b211d699a8e3d434df9
Keroly <15122112779@163.com>
2020-05-21 14:03:05 +0800

    字符串的所有排列

--- offer/J38.java
+++ offer/J38.java
@@ -6,6 +6,33 @@
 
  */
 
-public class J38 {
+import java.util.ArrayList;
+import java.util.HashSet;
 
+public class J38 {
+    public void swap(char[] s, int i, int j){
+        char c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+    public void process(char[] s, int index, ArrayList<String> list){
+        if (index == s.length - 1) {
+            list.add(String.valueOf(s));
+            return;
+        }
+        HashSet<Character> set = new HashSet();
+        for (int i = index; i < s.length; i++) {
+            if (!set.contains(s[i])){
+                set.add(s[i]);
+                swap(s, i, index);
+                process(s, index + 1, list);
+                swap(s, i, index);
+            }
+        }
+    }
+    public String[] permutation(String s) {
+        ArrayList<String> list = new ArrayList<String>();
+        process(s.toCharArray(), 0, list);
+        return list.toArray(new String[list.size()]);
+    }
 }
