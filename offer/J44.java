200faccde83318f2824cbf7f9b5a2917b315afd5
Keroly <15122112779@163.com>
2020-05-23 20:04:43 +0800

    数字序列中某一位的数字

---
+++ offer/J44.java
@@ -0,0 +1,25 @@
+/*
+    数字序列中某一位的数字
+
+题目描述：
+数字以0123456789101112131415…的格式序列化到一个字符序列中。在这个序列中，第5位（从下标0开始计数）是5，第13位是1，第19位是4，等等。请写一个函数，求任意第n位对应的数字。
+
+ */
+
+public class J44 {
+    public int findNthDigit(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int i = 1;
+        while (n > Math.pow(10, i - 1) * 9) {
+            n -= Math.pow(10, i - 1) * 9;
+            i++;
+        }
+        int num = (int) Math.pow(10, i - 1) + (n - 1) / i;
+        int index = (n - 1) % i ;
+        String res = String.valueOf(num);
+        String s = res.substring(index, index + 1);
+        return Integer.parseInt(s);
+    }
+}
