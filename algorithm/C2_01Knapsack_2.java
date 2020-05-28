f430a39ea338f6646c78320b3657207a5a9f7a12
Keroly <15122112779@163.com>
2020-05-28 21:18:48 +0800

    01背包问题

---
+++ algorithm/C2_01Knapsack_2.java
@@ -0,0 +1,50 @@
+/*
+    01背包问题
+
+题目描述：
+有 N 件物品和一个容量是 V 的背包。每件物品只能使用一次。
+第 i 件物品的体积是 vi，价值是 wi。
+求解将哪些物品装入背包，可使这些物品的总体积不超过背包容量，且总价值最大。输出最大价值。
+
+输入格式：
+第一行两个整数，N，V，用空格隔开，分别表示物品数量和背包容积。
+接下来有 N 行，每行两个整数 vi,wi，用空格隔开，分别表示第 i 件物品的体积和价值。
+
+输出格式：
+输出一个整数，表示最大价值。
+
+ */
+
+import java.util.Scanner;
+
+public class C2_01Knapsack_2 {
+
+    public static void main(String[] args) throws Exception { // 原始dp,一维滚动数组
+        Scanner reader = new Scanner(System.in);
+        // 物品的数量为N
+        int N = reader.nextInt();
+        // 背包的容量为V
+        int V = reader.nextInt();
+        // 一个长度为N的数组，第i个元素表示第i个物品的体积；
+        int[] v = new int[N + 1] ;
+        // 一个长度为N的数组，第i个元素表示第i个物品的价值；
+        int[] w = new int[N + 1] ;
+
+        for (int i = 1 ; i <= N ; i++){
+            v[i] = reader.nextInt();
+            w[i] = reader.nextInt();
+        }
+        reader.close() ;
+
+        int[] dp = new int[V + 1];
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = V; j - v[i] >= 0; j--)
+            {
+                dp[j] = Math.max(dp[j], dp[j - v[i]] + w[i]);
+            }
+        }
+        System.out.println(dp[V]);
+    }
+
+}
