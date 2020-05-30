c29434a296e39150604307bc748b0e008621cb73
Keroly <15122112779@163.com>
2020-05-30 18:13:10 +0800

    零钱兑换

--- leetcode/L518.java
+++ leetcode/L518.java
@@ -13,5 +13,16 @@
  */
 
 public class L518 {
-
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= coins.length; i++)
+        {
+            for (int j = coins[i - 1]; j <= amount; j++)
+            {
+                dp[j] = dp[j] + dp[j - coins[i - 1]] ;
+            }
+        }
+        return dp[amount];
+    }
 }
