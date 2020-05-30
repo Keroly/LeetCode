5665e34794ace92c5ff2dd16bc3bb4bb706c8849
Keroly <15122112779@163.com>
2020-05-30 18:53:43 +0800

    三角形最小路径和

--- leetcode/L120.java
+++ leetcode/L120.java
@@ -3,7 +3,7 @@
 
 题目描述：
 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
-相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。 空间复杂度O(n)
+相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。 空间复杂度O(n)（n 为三角形的总行数）
 
  */
 
@@ -11,30 +11,21 @@ import java.util.List;
 
 public class L120 {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle.size() == 0) {
-            return 0;
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i < n; i++)
+        {
+            dp[i + 1] = triangle.get(n - 1).get(i);
         }
-        int res = Integer.MAX_VALUE;
-        int[] dp = new int[triangle.get(triangle.size() - 1).size()];
-        dp[0] = triangle.get(0).get(0);
-        for (int i = 1; i < triangle.size(); i ++) {
-            int pre = dp[0];
-            int cur = dp[0];
-            for (int j = 0; j <= i; j++) {
-                if (j > 0 && i != j) {
-                    cur = dp[j];
-                    dp[j] = Math.min(dp[j], pre) + triangle.get(i).get(j);
-                    pre = cur;
-                }else if (j == 0) {
-                    dp[j] = dp[j] + triangle.get(i).get(j);
-                }else {
-                    dp[j] = pre + triangle.get(i).get(j);
-                }
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                dp[j] = Math.min(dp[j],dp[j + 1]) + triangle.get(i - 1).get(j - 1);
             }
         }
-        for (Integer element : dp) {
-            res = Math.min(res, element);
-        }
-        return res;
+        return dp[1];
     }
 }
