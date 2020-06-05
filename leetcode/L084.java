b1ab6780958b1ecdf18e7283144bbd31a38bb4ae
Keroly <15122112779@163.com>
2020-06-05 14:51:31 +0800

    柱状图中的最大矩形

--- leetcode/L084.java
+++ leetcode/L084.java
@@ -4,6 +4,7 @@
 题目描述:
 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+
  */
 
 public class L084 {
@@ -32,10 +33,6 @@ public class L084 {
     public int largestRectangleArea(int[] height) {
         if (height.length == 0) return 0;
         return process(height, 0, height.length - 1);
-    }
-
-    public static void main(String[] args) {
-        System.out.printf("" + new L084().largestRectangleArea(new int[]{0, 2, 0}));
     }
 
 }
