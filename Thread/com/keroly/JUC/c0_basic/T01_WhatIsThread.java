89afd5119eddc6520006973a55d407c0bfe709e8
Keroly <15122112779@163.com>
2020-06-21 20:30:49 +0800

    WhatIsThread

--- Thread/com/keroly/JUC/c0_basic/T01_WhatIsThread.java
+++ Thread/com/keroly/JUC/c0_basic/T01_WhatIsThread.java
@@ -2,7 +2,6 @@ package com.keroly.JUC.c0_basic;
 
 import java.util.concurrent.TimeUnit;
 
-
 public class T01_WhatIsThread {
 
     private static class T1 extends Thread {
@@ -20,6 +19,7 @@ public class T01_WhatIsThread {
     }
 
     public static void main(String[] args) {
+        //new T1().run();
         new T1().start();
         for (int i = 0; i < 10; i++) {
             try {
