cc309577ce553b60cfc6b2aa936ca818c1dccc50
Keroly <15122112779@163.com>
2020-06-21 19:40:58 +0800

    commit

---
+++ Thread/com/keroly/JUC/c0_basic/T02_Sleep_Yield_Join.java
@@ -0,0 +1,89 @@
+/*
+ * File Name:T02_Sleep_Yield_Join is created on 2020-04-21 15:26 by tangwan
+ *
+ * Copyright (c) 2020, tangwan All Rights Reserved.
+ *
+ */
+package com.keroly.JUC.c0_basic;
+
+/**
+ * @author tangwan
+ * @Description : T02_Sleep_Yield_Join
+ * @date 2020-04-21 15:26
+ * @since JDK 1.8
+ */
+public class T02_Sleep_Yield_Join {
+    public static void main(String[] args) {
+        //testSleep();
+        //testYield();
+        testJoin();
+    }
+
+    private static void testJoin() {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("A" + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < 10; i++) {
+                System.out.println("B" + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    private static void testYield() {
+
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("A " + i);
+                if (i % 10 == 0) {
+                    Thread.yield();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("B " + i);
+                if (i % 10 == 0) {
+                    Thread.yield();
+                }
+            }
+        }).start();
+    }
+
+    private static void testSleep() {
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Sleep " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+}
