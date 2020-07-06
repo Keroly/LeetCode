1f84c942f5d34a571e5aa9785282f5c9ae70eb6a
Keroly <15122112779@163.com>
2020-07-06 08:57:41 +0800

    斐波那契数列

--- offer/J10_1.java
+++ offer/J10_1.java
@@ -2,7 +2,7 @@
     斐波那契数列(n)
 
 题目描述：
-答案需要取模 1e9+7（1000000007）
+0,1,1,2,...   答案需要取模 1e9+7（1000000007）
 
  */
 
@@ -11,14 +11,17 @@ public class J10_1 {
         if (n < 2) {
             return n;
         }
+
         int a = 0;
         int b = 1;
         int sum = 0;
-        while (n-- > 1){
+        while (n-- >= 2) {
             sum = (a + b) % 1000000007;
-            a = b;
+            b = a + b;
+            a = b - a;
             b = sum;
         }
+
         return b;
     }
 }
