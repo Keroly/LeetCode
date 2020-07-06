3c2ca842dbddc3755945882cecc081b4c5eef8b5
Keroly <15122112779@163.com>
2020-07-06 09:55:13 +0800

    矩阵中的路径

--- offer/J12.java
+++ offer/J12.java
@@ -8,31 +8,29 @@
  */
 
 public class J12 {
-    public boolean process(char[][] board, int i, int j, String word, int index, int[][] flag){
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || flag[i][j] == 1 || board[i][j] != word.charAt(index)){
+    public boolean process(char[][] board,int[][] flag, int i, int j, String word){
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || flag[i][j] == 1 || word.charAt(0) != board[i][j]) {
             return false;
         }
 
-        if (index == word.length() - 1){
+        if (word.length() == 1) {
             return true;
         }
 
         flag[i][j] = 1;
-        boolean res = process(board, i + 1, j, word, index + 1, flag)
-                || process(board, i - 1, j, word, index + 1, flag)
-                || process(board, i, j + 1, word, index + 1, flag)
-                || process(board, i, j - 1, word, index + 1, flag);
+        String str = word.substring(1);
+        boolean res = process(board, flag, i, j - 1, str) || process(board, flag, i, j + 1, str) || process(board, flag, i - 1, j, str) || process(board, flag, i + 1, j, str);
         flag[i][j] = 0;
+
         return res;
     }
 
     public boolean exist(char[][] board, String word) {
-        int x = board.length;
-        int y = board[0].length;
-        for (int i = 0; i < x; i++){
-            for (int j = 0; j < y; j++){
-                int[][] flag = new int[x][y];
-                if (process(board, i, j, word, 0, flag)){
+        int[][] flag = new int[board.length][board[0].length];
+        boolean res = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0) && process(board, flag, i, j, word) == true) {
                     return true;
                 }
             }
