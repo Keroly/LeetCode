f92291ea79d43c51b33175beaea0bad3482da4cc
Keroly <15122112779@163.com>
2020-07-09 19:52:06 +0800

    commit

--- Thread/com/keroly/JUC/c1_singleton/Mgr03.java
+++ Thread/com/keroly/JUC/c1_singleton/Mgr03.java
@@ -5,11 +5,11 @@ package com.keroly.JUC.c1_singleton;
     虽然达到了按需求初始化的目的，但是却带来了线程不安全的问题
     可以通过双重检查解决
 
-
  */
 
 public class Mgr03 {
     private /*volatile*/ static Mgr03 INSTANCE;
+
     // 在超高并发的场景下，使用volatile禁止指令重排序，一般测不出来
 
     private Mgr03() {};
