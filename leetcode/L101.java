a7c9ead6eccb66d10fb9026542c9bc41b0d474e9
keroly <15122112779@163.com>
2020-07-28 09:06:23 +0800

    对称二叉树

--- leetcode/L101.java
+++ leetcode/L101.java
@@ -6,7 +6,11 @@
 
  */
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class L101 {
     public class TreeNode {
@@ -19,12 +23,9 @@ public class L101 {
 
     public boolean process(TreeNode node1, TreeNode node2){
         if (node1 == null && node2 == null) return true;
-        if (node1 == null || node2 == null) return false;
-        if (node1.val == node2.val){
-            return process(node1.left, node2.right) && process(node1.right, node2.left) ;
-        }
-        return false;
-    }
+        if (node1 == null || node2 == null || node1.val != node2.val) return false;
+        return process(node1.left, node2.right) && process(node1.right, node2.left);
+     }
 
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
@@ -38,19 +39,16 @@ public class L101 {
         LinkedList<TreeNode> list = new LinkedList<>();
         list.add(root);
         list.add(root);
-        while (!list.isEmpty()){
-            TreeNode node1 = list.pop();
-            TreeNode node2 = list.pop();
-            if (node1 == null && node2 == null) continue;
-            if (node1 == null || node2 == null) return false;
-            if (node1.val != node2.val) return false;
-
-            list.add(node1.left);
-            list.add(node2.right);
-            list.add(node1.right);
-            list.add(node2.left);
+        while (!list.isEmpty()) {
+            TreeNode left = list.poll();
+            TreeNode right = list.poll();
+            if (left == null && right == null) continue;
+            if (left == null || right == null || left.val != right.val) return false;
+            list.add(left.left);
+            list.add(right.right);
+            list.add(left.right);
+            list.add(right.left);
         }
         return true;
     }
-
 }
