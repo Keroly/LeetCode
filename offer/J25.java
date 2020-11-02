1851316e52440d021bd2762930f69dc4a14f7652
keroly <15122112779@163.com>
2020-11-02 16:48:52 +0800

    合并两个排序的链表

--- offer/J25.java
+++ offer/J25.java
@@ -14,26 +14,21 @@ public class J25 {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null){
+        if (l1 == null) {
             return l2;
-        }else if (l2 == null) {
+        }
+        if (l2 == null) {
             return l1;
         }
-        ListNode head = l1;
-        if (l1.val > l2.val){
-            head = l2;
-            l2 = l2.next;
-        }else {
-            l1 = l1.next;
-        }
-        ListNode cur = head;
+        ListNode node = new ListNode(-1);
+        ListNode cur = node;
         while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            }else {
+            if (l1.val >= l2.val) {
                 cur.next = l2;
                 l2 = l2.next;
+            }else {
+                cur.next = l1;
+                l1 = l1.next;
             }
             cur = cur.next;
         }
@@ -43,6 +38,6 @@ public class J25 {
         if (l2 != null) {
             cur.next = l2;
         }
-        return head;
+        return node.next;
     }
 }
