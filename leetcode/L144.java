2bfc0e19c0114d392e40b57b967b74b9e976d813
keroly <15122112779@163.com>
2020-11-03 15:57:00 +0800

    二叉树的前序遍历

---
+++ leetcode/L144.java
@@ -0,0 +1,46 @@
+/*
+    二叉树的前序遍历
+
+题目描述：
+使用非递归,进行二叉树的前序遍历。
+
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class L144 {
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+}
