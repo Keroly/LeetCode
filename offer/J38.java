40d644a7f643a57b6400efd0af89b2a2130878f8
keroly <15122112779@163.com>
2020-11-05 09:47:40 +0800

    字符串的所有排列

--- offer/J38.java
+++ offer/J38.java
@@ -10,29 +10,34 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class J38 {
-    public void swap(char[] s, int i, int j){
-        char c = s[i];
-        s[i] = s[j];
-        s[j] = c;
+    public void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    public void process(char[] s, int index, ArrayList<String> list){
-        if (index == s.length - 1) {
-            list.add(String.valueOf(s));
+    public void dfs(char[] arr, int index, ArrayList<String> res) {
+        if (index == arr.length - 1) {
+            res.add(new String(arr));
             return;
         }
-        HashSet<Character> set = new HashSet();
-        for (int i = index; i < s.length; i++) {
-            if (!set.contains(s[i])){
-                set.add(s[i]);
-                swap(s, i, index);
-                process(s, index + 1, list);
-                swap(s, i, index);
+        HashSet set = new HashSet();
+        for (int i = index; i < arr.length; i++) {
+            if (!set.contains(arr[i])) {
+                set.add(arr[i]);
+                swap(arr, i, index);
+                dfs(arr, index + 1, res);
+                swap(arr, i, index);
             }
         }
     }
     public String[] permutation(String s) {
-        ArrayList<String> list = new ArrayList<String>();
-        process(s.toCharArray(), 0, list);
-        return list.toArray(new String[list.size()]);
+        char[] arr = s.toCharArray();
+        ArrayList<String> res = new ArrayList<String>();
+        dfs(arr, 0, res);
+        String[] ans = new String[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            ans[i] = res.get(i);
+        }
+        return ans;
     }
 }
