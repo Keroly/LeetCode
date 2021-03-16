01c5b58e7299d11096c73555a239a61a34dc1bb1
Keroly <15122112779@163.com>
2021-03-16 14:39:04 +0800

    重建二叉树

--- offer/J07.java
+++ offer/J07.java
@@ -14,28 +14,27 @@ public class J07 {
         TreeNode(int x) { val = x; }
     }
 
-    public TreeNode process(int[] preorder, int prei, int prej, int[] inorder, int ini, int inj) {
-        if (prei > prej) {
+    public TreeNode dfs(int[] preorder, int prei, int prej, int[] inorder, int ini, int inj) {
+        if (prei > prej){
             return null;
         }
-        TreeNode ans = new TreeNode(preorder[prei]);
+        TreeNode root = new TreeNode(preorder[prei]);
         int flag = ini;
         for (int i = ini; i <= inj; i++) {
-            if (inorder[i] == preorder[prei]) {
+            if (preorder[prei] == inorder[i]) {
                 flag = i;
                 break;
             }
         }
-        ans.left = process(preorder, prei + 1, prei + flag - ini, inorder, ini, flag - 1);
-        ans.right = process(preorder, prei + flag - ini + 1, prej, inorder, flag + 1, inj);
-        return ans;
+        root.left = dfs(preorder, prei + 1, prei + flag - ini, inorder, ini, flag - 1);
+        root.right = dfs(preorder, prei + flag - ini + 1, prej, inorder, flag + 1, inj);
+        return root;
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || preorder.length == 0) {
+        if (preorder == null || inorder == null || preorder.length == 0) {
             return null;
         }
-        TreeNode root = process(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
-        return root;
+        return dfs(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 }
