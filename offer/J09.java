71b154aa11b756d52d53a412687cb1872ac9b5a3
keroly <15122112779@163.com>
2021-03-20 12:38:09 +0800

    两个栈实现队列

--- offer/J09.java
+++ offer/J09.java
@@ -3,7 +3,7 @@
 
 题目描述：
 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。
-(若队列中没有元素，deleteHead 操作返回 -1 )
+(若队列中没有元素，deleteHead操作返回 -1 )
 
  */
 
@@ -34,5 +34,4 @@ public class J09 {
         }
         return s2.pop();
     }
-    
 }
