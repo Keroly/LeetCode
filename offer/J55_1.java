531e33fe0728546d7455e64590557d0920a6fa0f
Keroly <15122112779@163.com>
2021-03-25 21:50:40 +0800

    二叉树的深度

--- offer/J55_1.java
+++ offer/J55_1.java
@@ -17,12 +17,9 @@ public class J55_1 {
     }
 
     public int maxDepth(TreeNode root) {
-        if (root == null){
+        if (root == null) {
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-
-        return Math.max(left, right) + 1;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
