708515897f55ab212d250de73994aff3fb52eacc
Keroly <15122112779@163.com>
2021-03-30 20:06:27 +0800

    二叉搜索树的最近公共祖先

--- offer/J68_1.java
+++ offer/J68_1.java
@@ -21,10 +21,10 @@ public class J68_1 {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val < p.val && root.val < q.val){
+        if (p.val > root.val && q.val > root.val) {
             return lowestCommonAncestor(root.right, p, q);
         }
-        if(root.val > p.val && root.val > q.val){
+        if (p.val < root.val && q.val < root.val) {
             return lowestCommonAncestor(root.left, p, q);
         }
         return root;
